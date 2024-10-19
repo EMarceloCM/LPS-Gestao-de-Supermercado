0f2ee25d8bf08e699edf1229f8ae2e1f42278362
exd02 <eliamxd1337@gmail.com>
2024-10-19 19:06:09 -0300

    Sprint01 - Implementação da ViewMain

---
+++ sigma-market/src/main/java/view/FrMainView.java
@@ -0,0 +1,23 @@
+package view;
+
+import javax.swing.*;
+
+public class FrMainView {
+    private JPanel panMain;
+    private JTable grdProducts;
+    private JLabel lblNome;
+    private JLabel lblMenu;
+    private JPanel panTop;
+    private JPanel panLeft;
+    private JPanel panMenuTitle;
+    private JButton btnUsuario;
+    private JPanel panMenuItems;
+    private JButton btnFornecedor;
+    private JButton btnProduct;
+    private JButton btnEndereço;
+    private JButton btnRelOrders;
+    private JButton btnRelFeedbacks;
+    private JButton btnOrders;
+    private JButton btnLogIn;
+    private JButton btnSignIn;
+}
