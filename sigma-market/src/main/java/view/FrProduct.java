12ef4a05cd7a2ba213815f4d8705bebc05b9a4b7
exd02 <eliamxd1337@gmail.com>
2024-10-23 13:25:06 -0300

    Sprint 02 - Implementação da ViewProduto

---
+++ sigma-market/src/main/java/view/FrProduct.java
@@ -0,0 +1,38 @@
+package view;
+
+import javax.swing.*;
+
+public class FrProduct {
+    private JPanel panTop;
+    private JLabel lblTitle;
+    private JPanel panButtons;
+    private JButton btnNew;
+    private JButton btnCancel;
+    private JButton btnEdit;
+    private JButton btnDelete;
+    private JButton btnSalvar;
+    private JPanel panForm;
+    private JPanel panFormCore;
+    private JLabel lblexpDate;
+    private JLabel lblName;
+    private JTextField edtName;
+    private JLabel lblSKU;
+    private JLabel lblPrice;
+    private javax.swing.JScrollPane JScrollPane;
+    private JTable grd;
+    private JPanel panBot;
+    private JPanel panSearch;
+    private JTextField edtSearch;
+    private JLabel lblSearch;
+    private JLabel lblSearchImg;
+    private JPanel panMain;
+    private JFormattedTextField fEdtData;
+    private JTextField edtSKU;
+    private JFormattedTextField fEdtPrice;
+    private JLabel lblStock;
+    private JSpinner spnStock;
+    private JLabel lblDescription;
+    private JTextArea textArea1;
+    private JLabel lblImgUrl;
+    private JTextField edtImgUrl;
+}
