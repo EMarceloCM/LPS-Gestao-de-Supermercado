d7dd86d4bdc32c9bfdcf8ef8c311f73f98c02a2a
exd02 <eliamxd1337@gmail.com>
2024-10-28 20:37:50 -0300

    Sprint 03 - Arrumando a view de acordo com a sessao

--- sigma-market/src/main/java/view/FrMainView.java
+++ sigma-market/src/main/java/view/FrMainView.java
@@ -1,19 +1,15 @@
 package view;
 
 import controller.ProductController;
+import model.enums.Role;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
+import java.util.Objects;
 
 public class FrMainView extends JFrame {
     private JPanel panMain;
@@ -23,20 +19,21 @@ public class FrMainView extends JFrame {
     private JPanel panTop;
     private JPanel panLeft;
     private JPanel panMenuTitle;
-    private JButton btnUsuario;
+    private JButton btnUsers;
     private JPanel panMenuItems;
-    private JButton btnFornecedor;
+    private JButton btnSupplier;
     private JButton btnProduct;
     private JButton btnEndereco;
     private JButton btnRelOrders;
     private JButton btnRelFeedbacks;
     private JButton btnOrders;
-    private JButton btnLogIn;
+    private JButton btnLoginOrLogout;
     private JButton btnCart;
     private JButton btnPromotion;
     private JPanel panTopLeft;
     private JPanel panReference;
     private JLabel lblReference;
+    private JPanel panMenuAdmin;
 
     private ProductController controller;
 
@@ -52,6 +49,7 @@ public class FrMainView extends JFrame {
         controller = new ProductController();
 
         initCustomComponents();
+        changeViewBasedOnRole();
 
         controller.refreshBuyTable(grdProducts);
 
@@ -70,7 +68,7 @@ public class FrMainView extends JFrame {
                 super.mouseClicked(e);
             }
         });
-        btnFornecedor.addActionListener(new ActionListener() {
+        btnSupplier.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 FrSupplier dlg = new FrSupplier(FrMainView.this, true);
@@ -78,7 +76,7 @@ public class FrMainView extends JFrame {
                 dlg.setVisible(true);
             }
         });
-        btnUsuario.addActionListener(new ActionListener() {
+        btnUsers.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 FrCustomer dlg = new FrCustomer(FrMainView.this, true);
@@ -110,12 +108,28 @@ public class FrMainView extends JFrame {
                 dlg.setVisible(true);
             }
         });
-        btnLogIn.addActionListener(new ActionListener() {
+        btnLoginOrLogout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                FrLogin dlg = new FrLogin(FrMainView.this, true);
-                dlg.setLocationRelativeTo(FrMainView.this);
-                dlg.setVisible(true);
+                if (Objects.equals(btnLoginOrLogout.getText(), "Login")) {
+                    FrLogin dlg = new FrLogin(FrMainView.this, true);
+                    dlg.addWindowListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosed(WindowEvent e) {
+                            changeViewBasedOnRole();
+                        }
+
+                        @Override
+                        public void windowClosing(WindowEvent e) {
+                            changeViewBasedOnRole();
+                        }
+                    });
+                    dlg.setLocationRelativeTo(FrMainView.this);
+                    dlg.setVisible(true);
+                } else {
+                    Auth.SessionManager.Logout();
+                    changeViewBasedOnRole();
+                }
             }
         });
     }
@@ -125,11 +139,32 @@ public class FrMainView extends JFrame {
         Cursor hand = new Cursor(Cursor.HAND_CURSOR);
         lblReference.setCursor(hand);
         btnCart.setCursor(hand);
-        btnLogIn.setCursor(hand);
+        btnLoginOrLogout.setCursor(hand);
 
         // set table layout
         grdProducts.setDefaultEditor(Object.class, null);
         grdProducts.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         grdProducts.getTableHeader().setReorderingAllowed(false);
+    }
+
+    private void changeViewBasedOnRole() {
+        if (Auth.SessionManager.isLoggedIn()) {
+            if (Auth.SessionManager.getLoggedUserRole() == Role.ADMIN) {
+                panMenuAdmin.setVisible(true);
+            } else if (Auth.SessionManager.getLoggedUserRole() == Role.CUSTOMER) {
+                panMenuAdmin.setVisible(false);
+            }
+            panMenuItems.setVisible(true);
+            lblMenu.setText("Menu");
+            btnCart.setVisible(true);
+            btnLoginOrLogout.setText("Logout");
+            btnLoginOrLogout.setIcon(new ImageIcon(getClass().getResource("/icons/logout.png")));
+        } else {
+            lblMenu.setText("Realize login!");
+            btnCart.setVisible(false);
+            panMenuItems.setVisible(false);
+            btnLoginOrLogout.setText("Login");
+            btnLoginOrLogout.setIcon(new ImageIcon(getClass().getResource("/icons/login.png")));
+        }
     }
 }
